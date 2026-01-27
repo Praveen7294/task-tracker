13ec009c030564cf421925dc96d33dc71fdd7f42
Praveen Kumar <praveen729483@gmail.com>
2026-01-28 00:55:33 +0530

    Added a TaskRepository interface

---
+++ src/main/java/com/example/taskcli/repository/TaskRepository.java
@@ -0,0 +1,21 @@
+package com.example.taskcli.repository;
+
+import com.example.taskcli.model.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository {
+
+    Task save(Task task);
+
+    Optional<Task> findById(int id);
+
+    List<Task> findAll();
+
+    List<Task> findByStatus(String status);
+
+    void deleteById(int id);
+
+    int getNextId();
+}
