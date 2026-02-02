9827a3cff8f4719dd3dd8d11ec00b6a474db7bc1
Praveen Kumar <praveen729483@gmail.com>
2026-02-03 03:20:59 +0530

    Added TaskStorage class to read data from json file

--- src/main/java/com/example/taskcli/repository/model/TaskStorage.java
+++ src/main/java/com/example/taskcli/repository/model/TaskStorage.java
@@ -1,5 +1,28 @@
 package com.example.taskcli.repository.model;
 
+import com.example.taskcli.model.Task;
+
+import java.util.List;
+
 public class TaskStorage {
-    
+
+    private int lastId;
+    private List<Task> tasks;
+
+    public int getLastId() {
+        return lastId;
+    }
+
+    public void setLastId(int lastId) {
+        this.lastId = lastId;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
 }
