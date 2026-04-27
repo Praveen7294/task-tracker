4a3e5bfc5dd4318ba60348697e18fe3efb14b4e1
Praveen Kumar <praveen729483@gmail.com>
2026-04-28 03:00:33 +0530

    Fix some issues

--- src/main/java/com/example/taskcli/TaskTrackerApplication.java
+++ src/main/java/com/example/taskcli/TaskTrackerApplication.java
@@ -1,8 +1,22 @@
 package com.example.taskcli;
 
+import com.example.taskcli.cli.CommandHandler;
+import com.example.taskcli.repository.FileTaskRepository;
+import com.example.taskcli.service.TaskService;
+
+import java.nio.file.Path;
+
 public class TaskTrackerApplication {
 
     public static void main(String[] args) {
-        // logic will implement later
+
+        Path filePath = Path.of("data", "tasks.json");
+
+        FileTaskRepository taskRepository = new FileTaskRepository(filePath);
+        TaskService service = new TaskService(taskRepository);
+
+        CommandHandler commandHandler = new CommandHandler(service, System.out, System.err);
+
+        commandHandler.handle(args);
     }
 }
